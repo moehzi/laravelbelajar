a2d4f659206f3449a1a65840a70578a1745f56b1
Muhammad Nur Faisi S <faizi1234098@gmail.com>
2020-06-02 15:44:25 +0800

    Tambah Database aja

---
+++ database/migrations/2020_06_02_073948_create_product_transaction_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductTransactionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_transaction', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('product_id');
+            $table->string('invoices_number');
+            $table->unsignedBigInteger('qty');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_transaction');
+    }
+}
